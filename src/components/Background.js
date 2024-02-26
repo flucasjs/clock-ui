120d5aeae5eb4798d584fc30d97b1356a5dcc652
unknown <flucasjs@gmail.com>
2024-02-25 23:23:17 -0800

    Update imports

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import Image from "next/image";
-import backgroundImages from "./backgroundImages";
+import backgroundImages from "../data/backgroundImages";
 
 export default function Background({ screen, theme }) {
   return (
--- src/components/Clock.js
+++ src/components/Clock.js
@@ -3,7 +3,7 @@
 import React from "react";
 import Image from "next/image";
 import { motion } from "framer-motion";
-import themeIcons from "./themeIcons";
+import themeIcons from "../data/themeIcons";
 
 function formatTime(date) {
   const options = {
